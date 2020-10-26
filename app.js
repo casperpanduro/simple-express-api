9cc6f8b729ed361dbd06f48ed99c012a8ba6e42b
Casper Panduro <cap@checkmate.dk>
2020-10-26 21:21:21 +0100

    quotes requests

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const app = express();
 
-app.get('/greetings', (req, res) => {
-	res.json({greeting: "Hello world"});
+// Send a GET request to /quotes to read a list of quotes
+app.get('/quotes', (req, res) => {
+	res.json(data);
 });
 
+// Send GET request to /quotes/:id read a quote
+// Send a GET request to /quotes to read a list of quotes
+app.get('/quotes/:id', (req, res) => {
+	const quote = data.quotes.find(quote => quote.id == req.params.id);
+	res.json(quote);
+});
+
+// Send POST request to /quotes to create new quote
+// Send PUT request to /quotes/:id to update quote
+// Send DELETE request to /quotes/:id to delete a quote
+// Send a GET request to /quotes/quote/random read a random quote
+
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
+
+const data = {
+    quotes: [
+      {
+        id: 8721,
+        quote: "We must accept finite disappointment, but we must never lose infinite hope.",
+        author: "Martin Luther King"
+      },
+      {
+        id: 5779,
+        quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
+        author: "Yvonne Pierre"
+      },
+      {
+        id: 3406,
+        quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
+        author: "Barbara DeAngelis"
+      }
+    ]
+  }
