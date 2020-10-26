118a678858a59d78120d7db60042a5e8437c8504
Casper Panduro <cap@checkmate.dk>
2020-10-26 22:53:39 +0100

    get random quote

--- routes.js
+++ routes.js
@@ -72,5 +72,9 @@ router.delete('/quotes/:id', asyncHandler(async (req, res, next) => {
 }));
 
 // Send a GET request to /quotes/quote/random read a random quote
+router.get('/quotes/quote/random', asyncHandler(async (req, res) => {
+	const quote = await records.getRandomQuote();
+	res.json(quote);
+}));
 
 module.exports = router;
